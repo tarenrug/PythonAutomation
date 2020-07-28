0757e233055839adf28783b42654b45f06b601fe
tarenrug <taren_rug@hotmail.co.uk>
2020-07-28 18:55:38 +0100

    Files move with this code

--- MovingAndRenamingFiles.py
+++ MovingAndRenamingFiles.py
@@ -6,5 +6,24 @@ import os
 import json
 import time
 
-class MyHandler():
-    taren = "hi"
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + "/" + filename
+            new_destination = folder_destination + "/" + filename
+            os.rename(src,new_destination)
+            print("My Handler is working")
+
+print("is this looping, yes it is?")
+folder_to_track = "C:/Users/taren/Desktop/TestingFolder/Folder1"
+folder_destination = "C:/Users/taren/Desktop/TestingFolder/Folder2"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler,folder_to_track,recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
--- test.py
+++ test.py
@@ -19,12 +19,13 @@ class Mapping:
     __method12=method1
 
 class MappingSubclass(Mapping):
-
-    def update(self, keys, values):
-        # provides new signature for update()
-        # but does not break __init__()
-        for item in zip(keys, values):
-            self.items_list.append(item)
+    # def update(self, keys, values):
+    #     # provides new signature for update()
+    #     # but does not break __init__()
+    #     for item in zip(keys, values):
+    #         self.items_list.append(item)
+    def taren(self):
+        a=1
 
 dogs=Mapping(["tennis1","tennis2"])
 dogs.method1("15")
@@ -36,7 +37,7 @@ print(dogs.items_list)
 
 cat = MappingSubclass(["n1","n2","n3"])
 
-cat.update(["extra3","extra4"],["values1","values2"])
+cat.update(["values1","values2"])
 cat._Mapping__method1("3")
 
 print(cat.items_list)
